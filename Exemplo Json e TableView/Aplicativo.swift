b2bc35ef27351f259f69f208f727bd4db53e6e0e
Usuário Convidado <Guest@L2602MICRO12.local>
2018-08-25 17:01:58 -0300

    Projeto 10

---
+++ Exemplo Json e TableView/Aplicativo.swift
@@ -0,0 +1,16 @@
+//
+//  Aplicativo.swift
+//  Exemplo Json e TableView
+//
+//  Created by Usuário Convidado on 25/08/2018.
+//  Copyright © 2018 Julio Augusto. All rights reserved.
+//
+
+import UIKit
+
+class Aplicativo: NSObject {
+    
+    var nome:String = ""
+    var imagem:String = ""
+
+}
